bd067b02c102bebdad2d1074f0899eacbaa97777
Alexey Staburov <gilbertfrost@yandex.ru>
2024-02-09 03:15:58 +0300

    photo done

--- src/js/app.js
+++ src/js/app.js
@@ -19,3 +19,11 @@ document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".parallax");
   var instances = M.Parallax.init(elems, {});
 });
+
+if (document.documentElement.clientWidth > 769) {
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, {});
+  });
+}
+
